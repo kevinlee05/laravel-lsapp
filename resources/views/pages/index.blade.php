bb4a2952631258894ab41abb357a94f4e71d88e5
unknown <kevinlee05@gmail.com>
2018-04-08 01:19:21 +0800

    login and register buttons in jumbotron

--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -1,6 +1,11 @@
 @extends('layouts.app')
 
 @section('content')
-<h1>{{ $title }} </h1>
-<p>This is the Laravel from Scratch tutorial by Brad Traversy</p>
+<div class="jumbotron text-center">
+    <h1>{{ $title }} </h1>
+    <p>This is the Laravel from Scratch tutorial by Brad Traversy</p>
+    <p><a href="/login" role="button" class="btn btn-primary btn-lg">Login</a>
+    <a href="/register" role="button" class="btn btn-success btn-lg">Register</a></p>
+</div>
+
 @endsection
