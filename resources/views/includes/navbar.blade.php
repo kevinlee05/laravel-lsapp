1072cbd64cc066c4276dfb35fa96a43addd00c9a
unknown <kevinlee05@gmail.com>
2018-04-08 18:32:52 +0800

    edit and delete post functionality

--- resources/views/includes/navbar.blade.php
+++ resources/views/includes/navbar.blade.php
@@ -16,7 +16,7 @@
                 <a class="nav-link" href="/services">Services</a>
             </li>
             <li class="nav-item">
-                <a class="nav-link" href="/posts">Posts</a>
+                <a class="nav-link" href="/posts">Blog</a>
             </li>
         </ul>
         <ul class="nav navbar-nav navbar-right">
--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -1,11 +1,17 @@
 @extends('layouts.app')
 
 @section('content')
-    <a href="/posts" class="btn btn-default">Back to Posts</a>
+    <a href="/posts" class="btn"><< Back to Posts</a>
     <h1>{{ $post->title }}</h1>
     <div>
         {!! $post->body !!}
     </div>
     <hr>
     <small>Written on {{$post->created_at}}</small>
+    <hr>
+    <a href="/posts/{{$post->id}}/edit" class="btn btn-primary">Edit Post</a>
+    {!!Form::open(['action' =>['PostsController@destroy', $post->id], 'method' => 'POST', 'class' => 'float-right'])!!}
+        {{Form::hidden('_method', 'DELETE')}}
+        {{Form::submit('Delete Post', ['class' => 'btn btn-danger'])}}
+    {!!Form::close()!!}
 @endsection 
